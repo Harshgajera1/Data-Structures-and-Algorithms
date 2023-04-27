3357b63787a52769ad59c4662258e7b4d2a68d66
harsh gajera <harshgajera202@gmail.com>
2023-04-27 23:00:07 +0530

    first commit

---
+++ day-3/demo.cpp
@@ -0,0 +1,17 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    int value;
+    value = cin.get();
+
+    if(48 <= value && 57 >= value){
+        cout << "This is number" << endl;
+    }
+    else if(97 <= value && value <= 122){
+        cout << "This is lowercase" << endl;
+    }else if(65 <= value && value <= 90){
+        cout << "This is Uppercase" << endl;
+    }
+}
