59bc7c3c0294c8700bf2cba180d90b02695e8214
harsh gajera <harshgajera202@gmail.com>
2023-04-28 22:39:36 +0530

    commit

--- day-4/pettern.cpp
+++ day-4/pettern.cpp
@@ -2,5 +2,51 @@
 using namespace std;
 
 int main() {
-    cout << "Good morning";
+    // ------------ 1 -------------
+    // int n,i=1;
+    // cin >> n;
+
+    // while(i<=n){
+    //     int j = 1;
+    //     while(j<=n){
+    //         cout << j;
+    //         // cout << n - j +1;
+    //         j++;
+    //     }
+    //     cout << endl;
+    //     i++;
+    // }
+
+
+    // ------------- 2 -------------
+    // int n,i=1,value =1;
+    // cin >> n;
+    // while (i<=n)
+    // {
+    //     int j = 1;
+    //     while(j<=n){
+    //         cout << value << ' ';
+    //         value++;
+    //         j++;
+    //     }
+    //     cout << endl;
+    //     i++;
+    // }
+
+    // ----------- 3 --------------
+    int n,i=1;
+    cin >> n;
+    while (i<=n)
+    {
+        int j=1;
+        while (j<=i)
+        {
+            cout << "*";
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+    
+    
 }
