2300cf4ef7ae335bde6468dd8541eba172901447
harsh gajera <harshgajera202@gmail.com>
2023-05-01 07:53:37 +0530

    commit

--- day-6/binary.cpp
+++ day-6/binary.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int main(){
+    // ------------- decimal to binary -------------
     // int n;
     // cin >> n;
 
@@ -11,31 +12,54 @@ int main(){
     // while (n != 0) {
     //     // for reminder
     //     int bit = n & 1;
-    //     // cout << n  << " " << bit << endl;
-    //     if(n<0){
-    //         ans = (!bit * pow(10,i)) + ans;
-    //     }else{
-    //         ans = (bit * pow(10,i)) + ans;
-    //     }
+
+    //     // for reverse
+    //     // ans = (bit * pow(10,i)) + ans;
+    //     // same flow
+    //     ans = (ans * 10) + bit;
+
     //     n = n >> 1; // n / 2
     //     i++;
     // }
     // cout << "Answer is " << ans << endl;
 
+    // ------------------- binary to decimal -----------------
     int n;
     cin >> n;
     int i=0, ans = 0;
     while (n!=0)
     {
-        int bit = n&1;
+        // enter value last bit
         int digit = n % 10;
-        // cout << bit << endl;
+
         if(digit == 1){
+            // with 2 to multiply beacuse decimal to binary we do */2.
             ans = ans + pow(2,i);
         }
+
         i++;
         n = n/10;
     }
     cout << ans << endl;
+
+
+    //------------ h.w -ve decimal to binary -------------- 
+    // int n,ans = 0,i=0;
+    // cin >> n;
+    // int value = n>0;
+    // if(n<0){
+    //     value = n * (-1);
+    // }else{ value = n; }
+
+    // while (value != 0)
+    // {
+    //     int bit = value & 1;
+    //     ans = (bit * pow(10,i)) + ans;
+    //     value = value >> 1;
+    //     i++;
+    //     value >> 1;
+    // }
+    // cout << "Binary number is " << ans;
+    
     
 }
---
+++ day-7/excercise.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main () {
+    int n,sum=0,i=0;
+    cout << "Enter value : ";
+    cin >> n;
+    if(n > pow(-2,31) || n > (pow(2,31)-1)){
+        cout << "456";
+    }
+
+    while (n!=0)
+    {
+        int digit = n%10;
+        // cout << digit << endl;
+        // sum = (digit * pow(10,i)) + sum;
+        sum = ( sum * 10 ) + digit;
+        i++;
+        n = n / 10;
+    }
+    cout << "Answer is " << sum;
+    
+}
