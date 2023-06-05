511893ba59d3cd57b6a2b0c7d1bb96e4f83cee01
harsh gajera <harshgajera202@gmail.com>
2023-06-05 23:23:33 +0530

    .

---
+++ day-12/binarySearch.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int binarySearch(int arr[],int size){
+    int start = 0;
+    int end = size-1;
+    int mid = (start+end)/2;
+}
+
+int main(){
+    int even[6] = {3,345,-32,-12,0,34};
+    int add[7] = {6,9,-12,56,32,234,89};
+}
