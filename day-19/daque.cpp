8510f412ea792534e29c2c0afad073f8a51ea0df
harsh gajera <harshgajera202@gmail.com>
2023-07-08 17:58:20 +0530

    commit

---
+++ day-19/daque.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<deque>
+using namespace std;
+
+int main(){
+    
+    deque<int> d;
+
+    d.push_back(54);
+    d.push_front(4);
+    d.push_front(2);
+
+    for(int i:d){
+        cout << i << " ";
+    }
+    cout << endl << endl;
+
+    cout << "first index value " << d.at(1) << endl << endl;
+
+    d.pop_back();
+
+    for(int i:d){
+        cout << i << " ";
+    }
+    cout << endl << endl;
+
+    d.erase(d.begin(),d.begin()+1);
+
+    cout << "Empty or not " << d.empty();
+}
